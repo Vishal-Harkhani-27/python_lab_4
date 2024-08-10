b6e84a16da8b75e41d46655318c6a0804554324d
Vishal-Harkhani-27 <bca2022vishal1754@tnraocollege.org>
2024-08-10 14:56:05 +0530

    Add files via upload

---
+++ dictionary_mtd.py
@@ -0,0 +1,16 @@
+emp = {
+    'id': 1,
+    'name': 'Vishal Harkhani',
+    'designation': 'Manager',
+    'salary': 60000
+}
+
+print("Dictionary:",emp)
+
+# Delete the 'designation' key
+del emp['designation']
+print("\nDeleting Designation:",emp)
+
+# Update the 'name' field
+emp['name'] = 'Vishu Patel'
+print("\nUpdating 'name':",emp)
